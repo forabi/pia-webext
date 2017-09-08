9c67f5fdd411d42e430a46d91be85a8aa4ee6862
Muhammad Fawwaz Orabi <mfawwaz93@gmail.com>
2017-09-08 15:42:43 +0300

    Remove unnecessary if

--- background/icon-handler.js
+++ background/icon-handler.js
@@ -1,29 +1,26 @@
 browser.storage.onChanged.addListener(async () => {
   const { serverKey, serverList, isLoggedIn, isDisabled } = await browser.storage.local.get(['isLoggedIn', 'serverKey', 'serverList', 'isDisabled']);
-  if (isDisabled !== undefined) {
-    console.log('Let\'s see', isDisabled);
-    if (!isDisabled && serverKey && serverList[serverKey]) {
-      browser.browserAction.setTitle({ title: `Connected to ${serverList[serverKey].name}` });
-      browser.browserAction.setBadgeText({ text: serverList[serverKey].iso });
-      browser.browserAction.setIcon({
-        path: {
-          16: '/images/icons/icon16.png',
-          32: '/images/icons/icon32.png',
-        }
-      });
-    } else {
-      browser.browserAction.setBadgeText({ text: '' });
-      browser.browserAction.setIcon({
-        path: {
-          16: '/images/icons/icon16red.png',
-          32: '/images/icons/icon32red.png',
-        }
-      });
-      if (isLoggedIn) {
-        browser.browserAction.setTitle({ title: 'Logged in but not connected' });
-      } else {
-        browser.browserAction.setTitle({ title: 'Not connected' });
+  if (!isDisabled && serverKey && serverList[serverKey]) {
+    browser.browserAction.setTitle({ title: `Connected to ${serverList[serverKey].name}` });
+    browser.browserAction.setBadgeText({ text: serverList[serverKey].iso });
+    browser.browserAction.setIcon({
+      path: {
+        16: '/images/icons/icon16.png',
+        32: '/images/icons/icon32.png',
       }
+    });
+  } else {
+    browser.browserAction.setBadgeText({ text: '' });
+    browser.browserAction.setIcon({
+      path: {
+        16: '/images/icons/icon16red.png',
+        32: '/images/icons/icon32red.png',
+      }
+    });
+    if (isLoggedIn) {
+      browser.browserAction.setTitle({ title: 'Logged in but not connected' });
+    } else {
+      browser.browserAction.setTitle({ title: 'Not connected' });
     }
   }
 });
